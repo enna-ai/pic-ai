8ebf074d6922097d0b4454750d54a803ab56cc68
Anne <92945218+vian-ai@users.noreply.github.com>
2023-02-04 21:17:43 -0700

    add post route to backend, edit image generator function in createpost

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,8 +16,31 @@ const CreatePost = () => {
     const [generatingImg, setGeneratingImg] = useState(false);
     const [loading, setLoading] = useState(false);
 
-    const generateImage = () => {
+    const generateImage = async () => {
+        if (form.prompt) {
+            try {
+                setGeneratingImg(true);
 
+                const response = await fetch('http://localhost:8080/api/v1/dalle', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        prompt: form.prompt,
+                    }),
+                });
+
+                const data = await response.json();
+                setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setGeneratingImg(false);
+            };
+        } else {
+            alert('Please provide proper prompt');
+        }
     };
 
     const handleSubmit = () => {
@@ -85,7 +108,7 @@ const CreatePost = () => {
                 <div className='mt-10'>
                     <p className='mt-2 text-[#666e75] text-[14px]'>Once you have created the image you want, you can share it with others in the community</p>
                     <button type='submit' className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'>
-                        {loading ? 'Sharing...': 'Sharing with the community'}
+                        {loading ? 'Sharing...' : 'Share with the community'}
                     </button>
                 </div>
             </form>
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
 import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
 
 const router = express.Router();
 
@@ -17,20 +20,19 @@ router.route('/').post(async (req, res) => {
     try {
         const { prompt } = req.body;
 
-        const aiRes = await openai.createImage({
+        const aiResponse = await openai.createImage({
             prompt,
             n: 1,
             size: '1024x1024',
             response_format: 'b64_json',
         });
 
-        const image = aiRes.data.data[0].b64_json;
-
+        const image = aiResponse.data.data[0].b64_json;
         res.status(200).json({ photo: image });
     } catch (error) {
-        console.log(error);
-        res.status(500).send(error?.response.data.error.message);
-    };
+        console.error(error);
+        res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+    }
 });
 
 export default router;

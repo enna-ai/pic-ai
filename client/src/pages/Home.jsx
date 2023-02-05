b96e69c740651ae47fe12e23652094d7ab7e7459
Anne <92945218+vian-ai@users.noreply.github.com>
2023-02-05 13:41:45 -0700

    fix post route, form input validation and card name styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
             setLoading(true);
             
             try {
-                const response = await fetch('https://pic-ai-dall-e.onrender.com/api/v1/posts', {
+                const response = await fetch('http://localhost:8080/api/v1/post', {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',

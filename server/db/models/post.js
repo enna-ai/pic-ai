12696b4ae8a48e5e75d4bd4e0c5c61eff41a23c0
Anne <92945218+vian-ai@users.noreply.github.com>
2023-02-04 20:24:36 -0700

    implemented openai api router for dall-e, created basic router for post and created mongo post schema

---
+++ server/db/models/post.js
@@ -0,0 +1,9 @@
+import { Schema, model } from 'mongoose';
+
+const Post = new Schema({
+    name: { type: String, required: true },
+    prompt: { type: String, required: true },
+    photo: { type: String, required: true },
+});
+
+export default model('Post', Post);

2e1ca6ba683d9a3eded912d0968251fcffd6450a
Anne <92945218+vian-ai@users.noreply.github.com>
2023-02-05 13:50:01 -0700

    edit api url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
             setLoading(true);
             
             try {
-                const response = await fetch('http://localhost:8080/api/v1/post', {
+                const response = await fetch('https://pic-ai-dall-e.onrender.com/post', {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
